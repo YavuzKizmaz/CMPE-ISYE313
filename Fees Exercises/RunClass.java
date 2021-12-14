9652dfdfb670c9c0e7b358edc04adccffa0ee965
Yavuz Kızmaz <yaavuzkizmaz@gmail.com>
2021-12-14 22:17:00 +0200

    Fees Exercises

---
+++ Fees Exercises/RunClass.java
@@ -0,0 +1,15 @@
+
+package runclass;
+
+
+public class RunClass {
+
+
+    public static void main(String[] args) {
+       
+        new GUIClass().setVisible(true);
+       
+        
+    }
+    
+}
